77ad571304064df43365f8cf88d9f970268ccf0e
DelyaAryanty <143785007+DelyaAryanty@users.noreply.github.com>
2026-08-18 20:27:55 +0700

    Update api.js

--- FrontEnd/src/services/api.js
+++ FrontEnd/src/services/api.js
@@ -1,14 +1,17 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:
+    "https://uaspemogramman-web2-secure-digital-library-unsi-production.up.railway.app/api",
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
